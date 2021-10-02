50ee567d8dc7ce7da97f5b629e0edbbf09b8dca5
Jakub Wilk <wilkjakub64@gmail.com>
2021-10-02 21:40:10 +0200

    Add routes for arbiter generation views

--- arbiter/urls.py
+++ arbiter/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ArbiterViewSet
+from .views import ArbiterViewSet, GenerateArbiters, get_arbiters_form
 
 router = routers.DefaultRouter()
 router.register("arbiter", ArbiterViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("generate-arbiters", GenerateArbiters.as_view(), name="generate-arbiters"),
+    path("get-arbiters-form", get_arbiters_form, name="get-arbiters-form")
 ]
