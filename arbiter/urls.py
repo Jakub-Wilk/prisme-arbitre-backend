8c15f2a15492b675cfb0c1a7e65d240a794264d4
Jakub Wilk <wilkjakub64@gmail.com>
2021-10-03 07:11:52 +0200

    Add new action's url to urlpatterns

--- arbiter/urls.py
+++ arbiter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ArbiterViewSet, GenerateArbiters, get_arbiters_form, UserViewSet
+from .views import ArbiterViewSet, GenerateArbiters, get_arbiters_form, UserViewSet, LoadArbitersFromJSON
 
 router = routers.DefaultRouter()
 router.register("arbiter", ArbiterViewSet)
@@ -9,5 +9,6 @@ router.register("user", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("generate-arbiters", GenerateArbiters.as_view(), name="generate-arbiters"),
-    path("get-arbiters-form", get_arbiters_form, name="get-arbiters-form")
+    path("get-arbiters-form", get_arbiters_form, name="get-arbiters-form"),
+    path("load-arbiters", LoadArbitersFromJSON.as_view(), name="load-arbiters")
 ]
