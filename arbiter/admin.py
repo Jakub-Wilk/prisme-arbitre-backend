f9d5567de4f429be3b2f058970f11188d3b803b3
Jakub Wilk <wilkjakub64@gmail.com>
2021-10-03 07:11:34 +0200

    Update admin to use new action

--- arbiter/admin.py
+++ arbiter/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ArbiterProfile, Court, Language, Experience, Specialization, Document, Location
+from .models import ArbiterProfile, Court, Language, Specialization, Document, Location
 from django.db.models import Sum
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -13,17 +13,12 @@ class LanguageInline(admin.TabularInline):
     model = Language.arbiter.through
 
 
-class ExperienceInline(admin.StackedInline):
-    model = Experience
-    extra = 0
-
-
 class SpecializationInline(admin.TabularInline):
     model = Specialization.arbiter.through
 
 
 class ArbiterProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "get_name", "verified", "email", "nationality", "active", "get_experience")
+    list_display = ("id", "get_name", "verified", "email", "nationality", "active")
     filter_horizontal = ("specializations",)
 
     def get_name(self, obj):
@@ -34,22 +29,21 @@ class ArbiterProfileAdmin(admin.ModelAdmin):
             pass
         return name
 
-    def get_experience(self, obj):
-        return obj.experience.aggregate(Sum("period"))['period__sum']
-
     get_name.short_description = "name"
-    get_experience.short_description = "Months of experience"
 
-    inlines = (LanguageInline, ExperienceInline, SpecializationInline, DocumentInline)
+    inlines = (LanguageInline, SpecializationInline, DocumentInline)
 
 def generate_random_arbiters(*args):
     return HttpResponseRedirect(reverse("get-arbiters-form"))
+
+def load_real_arbiters(*args):
+    return HttpResponseRedirect(reverse("load-arbiters"))
 
 
 admin.site.register(ArbiterProfile, ArbiterProfileAdmin)
 admin.site.register(Court)
 admin.site.register(Language)
-admin.site.register(Experience)
 admin.site.register(Specialization)
 admin.site.register(Location)
 admin.site.add_action(generate_random_arbiters)
+admin.site.add_action(load_real_arbiters)
