23f308d6f9d3c5f8ff37d7f3ceb842cfa4ae6961
Jakub Wilk <wilkjakub64@gmail.com>
2021-10-03 07:10:58 +0200

    Update models

--- arbiter/models.py
+++ arbiter/models.py
@@ -14,14 +14,19 @@ class ArbiterProfile(models.Model):
     photo = models.ImageField(blank=True, null=True, upload_to="photos/")
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="arbiter_profile", null=True, blank=True)
     verified = models.BooleanField(default=False)
+    degree = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
     location = models.ForeignKey("Location", on_delete=models.CASCADE, blank=True, null=True)
+    country = models.CharField(blank=True, null=True, max_length=255)
+    nationality = models.CharField(blank=True, null=True, max_length=255)
     first_name = models.CharField(max_length=255, null=True, blank=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
     court = models.ForeignKey("Court", related_name="arbiters", on_delete=models.CASCADE, null=True, blank=True)
     email = models.EmailField(blank=True, null=True)
-    nationality = models.CharField(blank=True, null=True, max_length=255)
     description = models.CharField(blank=True, null=True, max_length=255)
     active = models.BooleanField(default=False, blank=True, null=True)
+    birth_year = models.IntegerField(blank=True, null=True)
+    experience = models.CharField(blank=True, null=True, max_length=65535)
 
     def __str__(self):
         return f"{self.pk} - Arbiter Profile"
@@ -50,16 +55,6 @@ class Language(models.Model):
 class Specialization(models.Model):
     arbiter = models.ManyToManyField(ArbiterProfile, related_name="specializations", blank=True)
     name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
-class Experience(models.Model):
-    arbiter = models.ForeignKey(ArbiterProfile, related_name="experience", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    period = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
